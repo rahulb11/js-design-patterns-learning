0db7d7da218857d71d09f48401289607bc8101bb
rahulb11 <rahulbhandari11@gmail.com>
2017-07-08 12:16:17 -0500

    Premium Version - Basic Skeleton

---
+++ Cat Clicker Premium/js/script.js
@@ -0,0 +1,32 @@
+/*
+var count1 = 0;
+$('#image1').click(function(){
+    count1++;
+    $('#count1').text('Count: '+count1);
+});
+
+var count2 = 0;
+$('#image2').click(function(){
+    count2++;
+    $('#count2').text('Count: '+count2);
+    console.log(count2);
+});
+*/
+
+var cats = [
+    {
+        name: 'Susy',
+        url: 'images/cat1.jpg',
+        count: 0
+    },
+    {
+        name: 'Mary',
+        url: 'images/cat2.jpg',
+        count: 0
+    },
+    {
+        name: 'Kelly',
+        url: 'images/cat3.jpg',
+        count: 0
+    }
+];
