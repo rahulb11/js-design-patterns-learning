3cfc7f12c813435402c68fd027bf711cf688395e
rahulb11 <rahulbhandari11@gmail.com>
2017-07-08 16:09:24 -0500

    Premium Version - Working

--- Cat Clicker Premium/js/script.js
+++ Cat Clicker Premium/js/script.js
@@ -1,32 +1,44 @@
-/*
-var count1 = 0;
-$('#image1').click(function(){
-    count1++;
-    $('#count1').text('Count: '+count1);
-});
-
-var count2 = 0;
-$('#image2').click(function(){
-    count2++;
-    $('#count2').text('Count: '+count2);
-    console.log(count2);
-});
-*/
-
-var cats = [
+//cat object
+var cats =
+[
     {
-        name: 'Susy',
-        url: 'images/cat1.jpg',
-        count: 0
+        "name": 'Susy',
+        "url": 'images/cat1.jpg',
+        "count": 0
     },
     {
-        name: 'Mary',
-        url: 'images/cat2.jpg',
-        count: 0
+        "name": 'Mary',
+        "url": 'images/cat2.jpg',
+        "count": 0
     },
     {
-        name: 'Kelly',
-        url: 'images/cat3.jpg',
-        count: 0
+        "name": 'Kelly',
+        "url": 'images/cat3.jpg',
+        "count": 0
     }
 ];
+
+var HTMLimage = '<a onclick="changeCount(%i%)"><img src="%data%" alt="A cat"></a>';
+var HTMLlink = '<li><a href="#" onclick="changeDetails(%i%)">%data%</a></li>';
+
+//looping through the cats to create links for each
+for(i in cats){
+    formattedHTMLlink = HTMLlink.replace("%data%",cats[i].name);
+    formattedHTMLlink = formattedHTMLlink.replace("%i%",i);
+    $("#links").append(formattedHTMLlink);
+}
+
+//function to change the details of display area
+function changeDetails(i){
+    formattedHTMLimage = HTMLimage.replace("%data%",cats[i].url);
+    formattedHTMLimage = formattedHTMLimage.replace("%i%",i);
+    $("#image").html(formattedHTMLimage);
+    $("#name").text("Name: " + cats[i].name);
+    $("#count").text("Count: " + cats[i].count);
+}
+
+//function to change the count
+function changeCount(i){
+    cats[i].count++;
+    $("#count").text("Count: " + cats[i].count);
+}
